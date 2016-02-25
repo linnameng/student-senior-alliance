bc1fed66310fe481205db27c0b1d524c8367f40a
Linna Meng <linnameng@linnas-mbp.ad.unb.ca>
2016-02-25 15:16:39 -0400

    Finished searching/viewing posts/contacting users.

--- js/search.js
+++ js/search.js
@@ -1,13 +1,34 @@
 $(document).ready( function() {
-    $('#btn-search-jobs').click(function() {
+    $('body').on('click','#btn-search-jobs',function(){
      	var content_id = $("#category-select").val();
         $('#search-results').hide().html($(content_id).html()).show(500);
         return false;
     });
 
-    $('#btn-contact').click(function() {
+    $('body').on('click','#btn-contact',function(){
      	var content_id = $(this).attr('href');
-        $('#search').hide().html($(content_id).html()).show(500);
+        $('#search-current-page').hide().html($(content_id).html()).show(500);
+        $('#search-page-results').hide();
         return false;
-    });
+	});
+
+	$('body').on('click','#btn-contact-this-person',function(){
+     	var content_id = $(this).attr('href');
+        $('#search-current-page').hide().html($(content_id).html()).show(500);
+        return false;
+	});
+
+	$('body').on('click','#btn-send-message',function(){
+     	var content_id = $(this).attr('href');
+        $('#search-current-page').hide().html($(content_id).html()).show(500);
+        return false;
+	});
+
+	$('body').on('click','#btn-search-return',function(){
+     	var content_id = $(this).attr('href');
+        $('#search-current-page').hide().html($(content_id).html()).show(500);
+        $('#search-results').hide();
+        $('#category-select').val('default');
+        return false;
+	});
  });
